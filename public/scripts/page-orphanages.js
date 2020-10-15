8f2be8284120d0d9a3f9e8ce901662fd0ec09359
Thiago <thsousa.dev@outlook.com>
2020-10-14 22:09:12 -0300

    finalização da index inicio da orphanages.html

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,26 @@
+//Criação do Mapa
+const map = L.map('mapid').setView([-20.3629347,-40.3048321], 16);
+
+//adiciona titulo
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//Cria o icone de pin
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29.68],
+    popupAnchor: [170,2]
+})
+
+//Criar popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" /></a>')
+
+//Criar e marcar o pin
+L.marker([-20.3629347,-40.3048321], {icon})
+    .addTo(map)
+    .bindPopup(popup)
